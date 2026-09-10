0d0b9e33fdad0212eb08b6599f76178cdcd21e6b
chrisatron3000 <nachoson573@gmail.com>
2026-09-10 13:08:16 +0000

    add basic class skelly

---
+++ BasicClass.java
@@ -0,0 +1,21 @@
+public class BasicClass {
+
+    Public static void main(String[] args){
+
+        //atributes/ dada
+        //variables
+
+
+        // Constructor --- SPECIAL METHOD called every time you make an object
+        // Same name as the class followed by parenthesis.
+        // BasicClass()
+
+
+        // Behaviors / methods
+        // You can write as many as you want.
+
+    }
+
+
+
+}
