57119fb6d614b0abe2518431d27e1038071fc654
chrisatron3000 <nachoson573@gmail.com>
2026-09-15 12:23:08 +0000

    dog practice

---
+++ crashcourse/Dog.java
@@ -0,0 +1,76 @@
+package crashcourse;
+
+public class Dog {
+
+    String name;
+    String ownerName;
+    String breed;
+    int age;
+    double weight;
+    boolean isHungry;
+    boolean isHome;
+    int energyLevel;
+
+    public Dog(String name, String ownerName, String breed){
+
+        this.name = name;
+        this.ownerName = ownerName;
+        this.breed = breed;
+
+        age = 7;
+        weight = 42.6;
+        isHungry = false;
+        isHome = true;
+        energyLevel = 100;
+
+
+
+    }
+
+    public void bark(){
+
+        System.out.println(this.name + " Barked playfully");
+    }
+
+    public void scratch(){
+
+        System.out.println(this.name + " scratched around the room makeing a mess!");
+    }
+
+    public void bite(){
+
+        System.out.print(this.name + " started biteing their toys")
+
+    }
+
+    public void eat(){
+
+        isHungry = false;
+        weight += 5;
+
+    }
+
+    public void runAway(){
+
+        isHome =false;
+        energyLevel -= 15;
+
+    }
+
+    public void comeHome(){
+
+        isHome = true;
+
+    }
+
+    public void sleep(){
+
+        energyLevel = 100;
+    }
+
+    public void haveBirthday(){
+
+        age += 1;
+    }
+    
+}
