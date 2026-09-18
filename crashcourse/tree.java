83bd1d2c4770ca092548890d68d78fba3309f125
chrisatron3000 <nachoson573@gmail.com>
2026-09-18 01:33:12 +0000

    finish tree

--- crashcourse/tree.java
+++ crashcourse/tree.java
@@ -4,13 +4,13 @@ public class tree{
 
     //4 private variables 
     String specificVariety; //const
-    private double height;
-    private String barkColor; //const
+    private double height; // priv
+    private String barkColor; //const & priv
     Double RootLength;
     int age;
-    private int glucoseLevel;
+    private int glucoseLevel; // priv
     boolean isAlive;
-    private boolean isDanger; //const
+    private boolean isDanger; //const & priv
     String leafColor;
 
     public tree(String specificVariety, String barkColor, Boolean isDangerous){// 3 paramiters 3 attribus
@@ -20,7 +20,7 @@ public class tree{
         this.isDanger = isDangerous;
 
 
-        height = 15;
+        height = 5;
         RootLength = 3.5;
         age = 1;
         glucoseLevel = 100;
@@ -34,18 +34,62 @@ public class tree{
 
     }
 
-    public void absorb(){
+    // 5 behaviors
+    public void absorb(int oxygen ){ //parameter
+        System.out.println(specificVariety + " absorbed oxegen through their leaves");
+        glucoseLevel += 15;
+        oxygen --;
+
+        if (glucoseLevel > 100){
+
+            glucoseLevel = 100;
+        }
 
     }
 
     public void grow(){
+        // && is a symbol that makes both the requirments to be true
+        // if else 1
+        if(0 < glucoseLevel && glucoseLevel< 100){
+            height += 1;
+            System.out.println(specificVariety + " grew taller to a new height of " + height); //concatination
+        
+        } else {
+            System.out.println(specificVariety + " could not grow");
+        }
+        
+
+        glucoseLevel -= 10;
+
+        if (glucoseLevel < 0){
+            glucoseLevel = 0;
+        }
 
     }
-    public void expand(){
 
+
+    public void expand(){
+        // if else 2
+        if(0 < glucoseLevel && glucoseLevel< 100){
+            RootLength *= 1.5;
+            System.out.println(specificVariety + "expanded their roots to " + RootLength + " inches"); //concatination
+        
+        } else {
+            System.out.println(specificVariety + " could not grow");
+        }
+        
+
+        glucoseLevel -= 20;
+        
+
+        if (glucoseLevel < 0){
+            glucoseLevel = 0;
+        }
     }
     public void selfDestruct(){
         isAlive = false;
+        leafColor = "brown";
+        System.out.println(specificVariety + "has self destructed and died ):");
 
     }
 
